9caf304454f3e0af9f99af215d3c6a3c8c28d8e3
atiqbitstream <atiq.bitstream@gmail.com>
2025-03-06 15:58:18 +0500

    created user-service

---
+++ server.js
@@ -0,0 +1,8 @@
+const epxress = require('express')
+const app = express();
+
+app.get("/users",(req,res)=>{
+    res.json([{id:1, name: "atiq khan"},{id:2, name: "noman khan"}]);
+});
+
+app.listen(8080, ()=> console.log("User service running on port 8080"));
