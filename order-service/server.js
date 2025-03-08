85d603adbdaa065cccab2ccb5423d8b346340de2
atiqbitstream <atiq.bitstream@gmail.com>
2025-03-08 22:10:32 +0500

    adding another endpoint to order service

--- order-service/server.js
+++ order-service/server.js
@@ -5,4 +5,11 @@ app.get("/orders",(req,res)=>{
     res.json([{id:101, item: "laptop"},{id:102, item:"phone"}])
 });
 
+app.get("/expiredOrders",(req,res)=>{
+    res.json([{id:102, item: "Trackball"},{id:102, item:"windows xp"}])
+});
+
 app.listen(8080, ()=>console.log("order service running on port 8080"));
+
+
+
