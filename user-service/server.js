24b845762563a9fc290dc5edb8d43b326d19b2e8
atiqbitstream <atiq.bitstream@gmail.com>
2025-03-09 15:37:54 +0500

    new endpoint for user-service added

--- user-service/server.js
+++ user-service/server.js
@@ -5,4 +5,8 @@ app.get("/users",(req,res)=>{
     res.json([{id:1, name: "atiq khan"},{id:2, name: "noman khan"}]);
 });
 
+app.get("/newUsers",(req,res)=>{
+    res.json([{id:1, name: "Tallal khan"}, {id:2, name:"saleem khan"}])
+})
+
 app.listen(8080, ()=> console.log("User service running on port 8080"));
